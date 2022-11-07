7112bc0692b75860ef550a43459404dd59ad38f4
up202004187 <up202004187@up.pt>
2022-11-07 01:37:31 +0000

    baudrate = 19200

--- cable/cable.c
+++ cable/cable.c
@@ -15,7 +15,7 @@
 
 // Baudrate settings are defined in <asm/termbits.h>, which is
 // included by <termios.h>
-#define BAUDRATE B38400
+#define BAUDRATE B19200
 #define _POSIX_SOURCE 1 // POSIX compliant source
 #define FALSE 0
 #define TRUE 1
