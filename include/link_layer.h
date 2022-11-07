3b45a52e5ca5af8f4bb3075be35907bfa07cf25b
up202004187 <up202004187@up.pt>
2022-11-07 00:52:14 +0000

    max payload = 512

--- include/link_layer.h
+++ include/link_layer.h
@@ -21,7 +21,7 @@ typedef struct
 
 // SIZE of maximum acceptable payload.
 // Maximum number of bytes that application layer should send to link layer
-#define MAX_PAYLOAD_SIZE 256
+#define MAX_PAYLOAD_SIZE 512
 
 // MISC
 #define FALSE 0
